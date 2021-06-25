f902d743b49dac6bf5a8a2e713171c974b3dcfc0
CarlosBurrlos <carlosburroughs@gmail.com>
2021-06-25 10:52:42 -0400

    Began creating views for user to view SQL query data

--- MySQLClient/views.py
+++ MySQLClient/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
@@ -7,3 +7,20 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("SQLDB client\n.")
 
+def get(request, queryString):
+    # Here we wil setup a 'GET' request
+    # This is a general method and will pipe to different get methods
+
+
+
+
+    return HttpResponse("InformationGot")
+
+def set(request):
+    return HttpResponse("Note:: SAC is note handling SET requests at the moment\n")
+
+
+def verifyQueryString(queryString):
+
+# Filter by store, Filter by audit ID, Grab variance specifications for a single Audit ID
+# Then run just general SELECT, JOIN, ETC.
